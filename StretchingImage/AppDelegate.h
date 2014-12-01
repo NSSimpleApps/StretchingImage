5fce08f2796b78ca1daaee283cf52ae9e07be051
NSSimpleApps <ns.poluyanov.sergey@gmail.com>
2014-12-01 19:26:28 +0600

    Initial Commit

---
+++ StretchingImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StretchingImage
+//
+//  Created by neo on 01.12.14.
+//  Copyright (c) 2014 NSSimpleApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StretchingImageTests/StretchingImageTests.m
@@ -0,0 +1,40 @@
+//
+//  StretchingImageTests.m
+//  StretchingImageTests
+//
+//  Created by neo on 01.12.14.
+//  Copyright (c) 2014 NSSimpleApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface StretchingImageTests : XCTestCase
+
+@end
+
+@implementation StretchingImageTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
